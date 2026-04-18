b2ffc1e037b9bc42ecc0698513b7a8586c565349
1510Darshan <Darshanwalhe1510@gmail.com>
2026-04-18 23:59:52 +0530

    Update index.html and Api.js for deployment
    - Updated the script source in index.html to the new build version.
    - Changed the API_URL in Api.js to the production URL.
    - Added new JavaScript file index-Cv1sU0Nv.js to the assets.

--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'https://portfolio-swart-theta-iljumjifai.vercel.app/';
 
 const getAuthHeader = () => {
   const token = localStorage.getItem('adminToken');
